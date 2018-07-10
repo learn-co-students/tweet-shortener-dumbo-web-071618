edb625dc1230d86e1078f3b5225193661d816201
Rosa Tu <rosalina.tu@gmail.com>
2018-07-10 15:56:28 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def dictionary
+  dictionary = {"hello" => 'hi', 
+                "to" => '2', 
+                "two" => '2',
+                "too" => '2', 
+                "for" => '4', 
+                "four" => '4',
+                "be" => 'b',
+                "you" => 'u',
+                "at" => '@', 
+                "and" => '&'
+                 }
+end                 
+
+def word_substituter(tweet)
+  tweet.split(" ").collect do |wd| 
+    if dictionary.keys.include?(wd)
+      wd = dictionary[wd]
+    else
+      wd = wd
+    end
+  end.join(" ")
+end   
+
+    
