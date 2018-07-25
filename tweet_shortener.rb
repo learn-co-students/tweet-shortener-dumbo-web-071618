4d483225683425a013942428b81b45fc22762250
Malcome Fein <mfein90@gmail.com>
2018-07-25 20:46:00 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def dictionary
+  {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+
+def word_substituter(tweet)
+tweet.split('').keys.join(",")
+
+# array then string
+end
