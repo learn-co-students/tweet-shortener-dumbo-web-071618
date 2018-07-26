1f32fb6b3b6c69fd3db78540747eed3c8e9aaae8
Malcome Fein <mfein90@gmail.com>
2018-07-26 16:16:13 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code here.
 
 def dictionary
@@ -15,9 +16,45 @@ def dictionary
   }
 end
 
-
 def word_substituter(tweet)
-tweet.split('').keys.join(",")
-
-# array then string
+  tweetArray = tweet.split.collect do |word|
+    # binding.pry
+    # array 
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+      # binding.pry
+    else
+      word
+    end
+  end
+  tweetArray.join(" ")
+  # string
 end
+
+
+def bulk_tweet_shortener(tweets)
+  tweets.collect do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+    # tweetArray = tweet.split.count
+    tweetArray = tweet.length
+    #binding.pry
+  if tweetArray > 140
+    word_substituter(tweet)
+  else 
+    tweet
+  end
+end
+
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + ("...")
+  else
+    word_substituter(tweet)
+  end
+end
+
